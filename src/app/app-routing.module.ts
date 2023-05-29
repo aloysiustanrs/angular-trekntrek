eb09c85206828de22f887303a4dab17608efd2c3
Your Name <aloysiustanrs@gmail.com>
2023-05-30 01:29:44 +0800

    add dynamic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+import { HomeComponent } from './home/home.component';
+import { MobileComponent } from './mobile/mobile.component';
+
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    { path: '', component: HomeComponent },
+    { path: 'mobile', component: MobileComponent },
+  ],
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomeComponent,
+    MobileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
